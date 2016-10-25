444403421e080a58084e7633dc4dce2765961fcf
Carsten Brandt <mail@cebe.cc>
2016-10-25 22:38:34 +0200

    Update make-php-keyring.sh
    added PHP 7 keys

--- make-php-keyring.sh
+++ make-php-keyring.sh
@@ -10,7 +10,7 @@ KEYS="$KEYS 90D90EC1" # Julien Pauli <jpauli@php.net>
 KEYS="$KEYS 7267B52D" # David Soria Parra <dsp@php.net>
 KEYS="$KEYS 5DA04B5D" # Stanislav Malyshev (PHP key) <stas@php.net>
 KEYS="$KEYS FC9C83D7" # Johannes Schlüter <johannes@php.net>
+KEYS="$KEYS 9C0D5763" # Anatol Belski <ab@php.net>
+KEYS="$KEYS 33CFC8B3" # Ferenc Kovacs <tyrael@php.net>
 
 gpg --keyserver pgp.mit.edu --recv-keys $KEYS
-
-
